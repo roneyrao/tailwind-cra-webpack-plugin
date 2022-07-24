d5028a99b2f14fab3793da23a92b333759c43c15
Roney <roneyrao@hotmail.com>
2022-07-24 13:53:26 +0800

    Init publish

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # tailwind-cra-webpack-plugin
 Tailwindcss v3 webpack plugin for CRA (Create React App) v3/v4
+
+## Background
+- Tailwindcss v3 doesn't support postcss v7, which is shipped in Create React App v3 and v4
+- The official method to work with webpack also has issues
+  + Duplicate buildings when HMR
+  + It can not work properly with the cache plugin ([hard-source-webpack-plugin](https://www.npmjs.com/package/hard-source-webpack-plugin-fixed-hashbug))
+
+## Usage
+- install
+`npm install -D tailwindcss@latest postcss@latest autoprefixer@latest tailwind-cra-webpack-plugin`
+
+- apply
+```javascript
+// craco.config.js as example
+
+const { TailwindWebpackPlugin } = require('tailwind-cra-webpack-plugin')
+
+module.exports = {
+  webpack: {
+    plugins: [
+      // The css entry point
+      new TailwindWebpackPlugin(require.resolve('../src/index.css')),
+    ],
+  }
+}
+```
+
+## Notes
+Only tested in CRA3 (nodejs v14), works in v4 theoretically.
---
+++ index.js
@@ -0,0 +1,122 @@
+const postcss = require('postcss')
+
+const plugins = [ require('tailwindcss'), require('autoprefixer')]
+
+
+class TailwindWebpackPlugin {
+  static name = 'TailwindWebpackPlugin'
+  static Instance;
+
+  constructor(entry) {
+    this.constructor.Instance = this
+    this.entry = entry
+    this.forceRebuild = true // Always compile at startup in case any js changed
+    this.targetFolders = null // Filled by the loader
+    this.onChangeHandler = this.onChange.bind(this)
+  }
+
+  onChange(changedFiles) {
+    // Compile when any target js changed
+    this.forceRebuild = !!changedFiles.find(f => (
+      (f.endsWith('.js') || f.endsWith('.jsx'))
+      && this.targetFolders.find(folder => f.startsWith(folder))
+    ))
+  }
+
+  needRebuild(m) {
+    if (this.forceRebuild) {
+      return true
+    } else {
+      return m.needRebuildO.apply(m, Array.prototype.slice.call(arguments, 1))
+    }
+  }
+
+  watch(fs) {
+    const result = fs.watchO.apply(fs, Array.prototype.slice.call(arguments, 1))
+    fs.watcher.prependListener("aggregated", this.onChangeHandler)
+    return result
+  }
+
+  apply(compiler) {
+    const isDev = compiler.options.mode === "development"
+    if (isDev) {
+      // hook watch
+      const fs = compiler.watchFileSystem
+      fs.watchO = fs.watch
+      fs.watch = this.watch.bind(this, fs)
+    }
+
+    compiler.hooks.beforeCompile.tap(this.constructor.name, (params) => {
+      // inject loader
+      params.normalModuleFactory.hooks.afterResolve.tap(this.constructor.name, (data) => {
+        if (data.userRequest === this.entry) {
+          data.loaders.push({ loader: __filename });
+        }
+      });
+      if (isDev) {
+        // hook needRebuild
+        params.normalModuleFactory.hooks.module.tap(this.constructor.name, (m, data) => {
+          if (data.resource === this.entry
+            && data.userRequest !== this.entry // skip the module without loaders
+          ) {
+            m.needRebuildO = m.needRebuild
+            m.needRebuild = this.needRebuild.bind(this, m)
+          }
+        });
+      }
+    });
+  }
+}
+
+function TailwindLoader (css, map, meta) {  
+  const cb = this.async()
+  const options = {
+    from: this.resourcePath,
+    map: false // Nonsense of the tailwindcss source code
+  }
+
+  return postcss(plugins)
+    .process(css, options)
+    .then((result) => {
+      let { css, map, root, processor, messages } = result
+
+      result.warnings().forEach((warning) => {
+        const { text, line, file } = warning
+        this.emitWarning(new Error(`Warning: ${text}`, file, line))
+      })
+
+      // Fill each time in case the content is changed in tailwind.config.js 
+      const targetFolders = TailwindWebpackPlugin.Instance.targetFolders = []
+      messages.forEach((msg) => {
+        if (msg.type === 'dependency') {
+          this.addDependency(msg.file)
+        } else if (msg.type === 'dir-dependency') {
+          targetFolders.push(msg.dir)
+        }
+      })
+
+      if (!meta) {
+        meta = {}
+      }
+      meta.ast = {
+        type: 'postcss',
+        version: processor.version,
+        root
+      }
+      meta.messages = messages
+
+      cb(null, css, map, meta)
+      return null
+    }).catch((err) => {
+      if (err.file) {
+        this.addDependency(err.file)
+      }
+      return err.name === 'CssSyntaxError'
+        ? cb(new SyntaxError(err))
+        : cb(err)
+    })
+}
+
+TailwindLoader.TailwindWebpackPlugin = TailwindWebpackPlugin
+
+module.exports = TailwindLoader;
